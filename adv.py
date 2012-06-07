447153131ba24d5d2ab6ffc6c13540c7b98452c9
bminton <bminton>
2012-06-07 14:26:12 +0000

    can now get list of items

--- adv.py
+++ adv.py
@@ -1,14 +1,40 @@
 #!/usr/bin/env python 
 from __future__ import print_function
-from xml.etree.ElementTree import ElementTree
+from xml.etree.ElementTree import ElementTree,XML
 import sys
 
-print('l')
+debug=True
+if debug: 
+    stderr=sys.stderr
+else: 
+    stderr=file('/dev/null')
 
-tree=ElementTree(file=sys.stdin)
-for i in tree.iter('item'):
-    for j in i.getchildren():
-        t=j.text.strip()
-        print (j.tag,":",t,file=sys.stderr)
-        if j.tag == 'name':
-            print ('get ' + t)
+def get_items(tree):
+    items=[]
+    for i in tree.iter('item'):
+        for j in i.getchildren():
+            t=j.text.strip()
+            print (j.tag,":",t,file=stderr)
+            if j.tag == 'name':
+                items.append(t)
+    return items 
+
+
+
+# put the goggles in xml mode
+print ('sw xml')
+
+for line in file('multi.xml'):
+    l=line.strip()
+    print("read line: "+l,file=stderr)
+    if l=='<error>' or l=='<success>':
+        in_xml=True
+        buf='' #start a new buffer
+    if in_xml:
+        buf+=line
+    if l=='</error>' or l=='</success>':
+        in_xml=False
+        tree=ElementTree(XML(buf))
+        items=get_items(tree)
+
+
