f3bbbb2fc1580f7ae46974cd411b71cc5c0d5580
bminton <bminton>
2012-06-13 16:09:09 +0000

    added interact mode

--- adv.py
+++ adv.py
@@ -53,6 +53,18 @@ def build_partial(thing):
     print('attempting build of ' + thing + ' missing ', missing,file=sys.stderr)
 
 
+def pre_interact():
+    # this will put us to the corner of 54th St and Ridgewood Ct.
+    #print('starting pre_interact...',file=sys.stderr)
+    for line in file('junkroom.steps'):
+        print(line,end='')
+        #print (line,file=sys.stderr)
+    print('sw xml')
+    print ('l')
+    #print('done with pre_interact...',file=sys.stderr)
+
+
+
 deps={}
 deps['uploader']=['MOSFET','status LED','RS232 adapter','EPROM burner','battery']
 deps['downloader']=['USB cable','display','jumper shunt','progress bar','power cord']
@@ -61,8 +73,32 @@ if len(sys.argv) !=2:
     print('usage: ' + sys.argv[0] + ' filename',file=sys.stderr)
     exit()
 
-tree=ElementTree(file=sys.argv[1])
-items=list_items()
-for i in items:
-    if i in deps['uploader'] or i in deps['downloader']:
+if sys.argv[1]=='-':
+    interactive=True;
+
+    pre_interact()
+    
+    in_xml=False 
+    for line in sys.stdin:
+        l=line.strip()
+        print("read line: "+l,file=stderr)
+        if l=='<error>' or l=='<success>':
+            in_xml=True
+            buf='' #start a new buffer
+        if in_xml:
+            buf+=line
+        if l=='</error>' or l=='</success>':
+            in_xml=False
+            tree=ElementTree(XML(buf))
+            for i in list_items():
+                if i in deps['uploader'] or i in deps['downloader'] or i=='keypad':
+                    build(i)
+
+
+else:
+    tree=ElementTree(file=sys.argv[1])
+    interactive=False
+
+for i in list_items():
+    if i in deps['uploader'] or i in deps['downloader'] or i=='keypad':
         build(i)
