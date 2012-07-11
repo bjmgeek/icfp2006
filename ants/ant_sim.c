4392d762b4551714b961d4c929bc88d38f4aa3ab
Brian Minton <brian@minton.name>
2012-07-11 12:50:53 -0400

    starting an ant simulator, which should be faster than using umix

---
+++ ants/ant_sim.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc,char *argv[])
+{
+    FILE *f;
+
+    char puzname[1024];
+
+
+
+    f=fopen(argv[1],"r");
+
+
+
+    exit(EXIT_SUCCESS);
+}
