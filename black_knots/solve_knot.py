a729230e55edc07a150fc92fa27c14f974036da9
Brian Minton <brian@minton.name>
2012-07-25 12:59:50 -0400

    successfully parse input rules, and build lists of plinks and movements remaining

--- black_knots/solve_knot.py
+++ black_knots/solve_knot.py
@@ -2,13 +2,16 @@ import sys
 import array
 
 inputs={}
+output=[]
+
 for line in open(sys.argv[1]):
     l=line.split()
     inputs[int(l[0])]=eval(l[2])
 
-movements={}
-plinks={}
-for i in inputs:
-    movements[i]=inputs[i][0]-i #positive to the right, negative to the left
-    plinks[i]=inputs[i][1]
+width=1+max(inputs)
 
+movements_remaining={}
+plinks_remaining={}
+for i in inputs:
+    movements_remaining[i]=inputs[i][0]-i #positive to the right, negative to the left
+    plinks_remaining[i]=inputs[i][1]
