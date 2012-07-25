9268c63836bbb59af0b8ad067112e54eef556701
Brian Minton <brian@minton.name>
2012-07-25 16:52:09 -0400

    swap() now takes args in either order; documentation of plinks for sort()

--- black_knots/solve_knot.py
+++ black_knots/solve_knot.py
@@ -16,11 +16,18 @@ for i in rules:
     movements_remaining[i]=rules[i][0]-i #positive to the right, negative to the left
     plinks_remaining[i]=rules[i][1]
 
+# outputs code to swap output columns x and y.
+# The lower (leftmost) output column gets abs(x-y) plinks
+# the higher numbered (rightmost) column gets 0 plinks
+# all other columns between x and y get 1 plink
 def swap(x,y):
+    x,y=sorted((x,y))
     path=[]
     for z in xrange(x,y):
          path.append ('|' * (z) + '><' + '|' * (width-z-1))
     if abs(x-y) > 1: 
         path.extend(reversed(path[:-1]))
+
     for row in path: 
         print row 
+
