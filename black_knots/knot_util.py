763cfe6507148ad6c5f95363a041dcf21305c775
Brian Minton <brian@minton.name>
2014-05-13 15:52:34 -0400

    make compress function keep going until no more changes

--- black_knots/knot_util.py
+++ black_knots/knot_util.py
@@ -1,19 +1,21 @@
 def compress(grid):
     if len(grid) == 0: return []
-
-    for y in xrange (1,len(grid)):
-        l0 = list(grid[y-1])
-        l1 = list(grid[y])
-        for x in xrange(len(l0)-1):
-            if l0[x] == '|' and l0[x+1] == '|' and l1[x] == '>' and l1[x+1] == '<':
-                l0[x] = '>'
-                l0[x+1] = '<'
-                l1[x] = '|'
-                l1[x+1] = '|'
-        
-    grid[y-1]=''.join(l0)
-    grid[y]=''.join(l1)
-    
+    old=list(grid)
+    new=[]
+    while old != new:
+        for y in xrange (len(grid)-1):
+            l0 = list(grid[y])
+            l1 = list(grid[y+1])
+            for x in xrange(len(l0)-1):
+                if l0[x] == '|' and l0[x+1] == '|' and l1[x] == '>' and l1[x+1] == '<':
+                    l0[x] = '>'
+                    l0[x+1] = '<'
+                    l1[x] = '|'
+                    l1[x+1] = '|'
+            grid[y]=''.join(l0)
+            grid[y+1]=''.join(l1)
+        old=list(new)
+        new=list(grid)
     null_line = '|' * len(grid[0])
     if null_line in grid:
         for i in xrange(grid.count(null_line)):
